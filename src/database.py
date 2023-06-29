fb4429c29a1a19ad88653a6595d8537b11cadcd5
Thabo <87668525+mdmsn@users.noreply.github.com>
2023-06-29 22:31:12 +0100

    Update module imports

--- src/database.py
+++ src/database.py
@@ -1,9 +1,9 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 import re
-from dotenv import load_dotenv
 
 load_dotenv()
 
