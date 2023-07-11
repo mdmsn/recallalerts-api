b462dd0d55c0961067e54670d2172a936e8bd1e6
Thabo <87668525+mdmsn@users.noreply.github.com>
2023-07-11 22:49:19 +0100

    Update database

--- src/database.py
+++ src/database.py
@@ -1,17 +1,16 @@
 from dotenv import load_dotenv
+import os
+import re
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-import re
 
 load_dotenv()
 
 db_URL = os.getenv("DATABASE_URL") 
-
 if db_URL.startswith("postgres://"):
     db_URL = db_URL.replace("postgres://", "postgresql://", 1)
-
+    
 engine = create_engine(db_URL, connect_args={})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
